69a9293fc7162faf9d2c310583e10d9235a7ee63
Cameron <cameronkwalden@gmail.com>
2022-04-29 14:11:27 -0700

    adds bootstrap container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import CompletedTodos from "./components/CompletedTodos";
+import React from "react";
 import TodoForm from "./components/TodoForm";
-import Todos from "./components/Todos";
+import TodoList from "./components/TodoList";
+import CompletedTodos from "./components/CompletedTodos";
 
 const App = () => {
-
   return (
-    <>
-    <h1>Todos:</h1>
-    <TodoForm />
-    <Todos />
-    <CompletedTodos />
-    </>
-  )
-}
+    <div className="container bg-white p-4 mt-5">
+      <h1>My Todo List</h1>
+      <TodoForm />
+      <TodoList />
+      <CompletedTodos />
+    </div>
+  );
+};
 
 export default App;
