0b76ee8eda3ae45771a4cc542752eb2060f5dce1
Cameron <cameronkwalden@gmail.com>
2022-04-29 10:34:46 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import CompletedTodos from "./components/CompletedTodos";
+import TodoForm from "./components/TodoForm";
+import Todos from "./components/Todos";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>Todos:</h1>
+    <TodoForm />
+    <Todos />
+    <CompletedTodos />
+    </>
+  )
 }
 
 export default App;
