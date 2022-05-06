38e2c1b9642e39ffdfef669690229aed303eed8f
Cameron <cameronkwalden@gmail.com>
2022-05-05 17:37:52 -0700

    changes initialState

--- src/redux/todosSlice.js
+++ src/redux/todosSlice.js
@@ -16,24 +16,18 @@ export const getTodos = createAsyncThunk("todos/getTodos", async () => {
 
 const todosSlice = createSlice({
     name: 'todos',
-    initialState: [
-        { _id: 1, title: 'task1', completed: false },
-        { _id: 2, title: 'task2', completed: false },
-        { _id: 3, title: 'task3', completed: false }
-    ],
+    initialState: [],
     reducers: {
         //reducer recieves the action and current state of the slice
         addTodo: (state, action) => {
             //creating new todo based on action payload
             const newTodo = {
-                // id: new Date(Date.now().toString()),
                 _id: nanoid(),
                 title: action.payload.title,
                 completed: false
             };
             //pushes the new todo into the array. redux takes new state and saves to the store
             state.push(newTodo)
-            console.log(newTodo, 'this is your new todo')
         },
         toggleCompletedTodos: (state, action) => {
             const idx = state.findIndex(todo => todo._id === action.payload._id);
@@ -47,10 +41,7 @@ const todosSlice = createSlice({
         },
         //when thunk dispatches a fulfilled action, api call and thunk have dispatched action successfully
         [getTodos.fulfilled]: (state, action) => {
-            console.log('succesfully grabbed todos')
-            // console.log(action.payload.todos, 'this is A.P.R')
-            // console.log(state, 'this is state')
-            console.log(action, 'action')
+            console.log('succesfully grabbed todos');
             return action.payload.urlResponse;
         },
         [getTodos.rejected]: (state, action) => {
